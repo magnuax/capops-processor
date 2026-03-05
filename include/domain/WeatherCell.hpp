2207af968e1d00c55fa05291741a0e61896c669c
ingvildandas <ingvild.andas@outlook.com>
2026-03-05 09:08:27 +0100

    UPDATE: WeatherCell class

--- include/domain/WeatherCell.hpp
+++ include/domain/WeatherCell.hpp
@@ -1,6 +1,15 @@
-#pragma once 
+#pragma once
+
+#include <cstdint>
+#include <string>
+#include "domain/types/WeatherSeverity.hpp"
 
 class WeatherCell {
-private: 
-public: 
-}; 
+public:
+    WeatherCell(std::string sectorId, std::int64_t timestamp, WeatherSeverity severity); 
+
+private:
+    std::string sectorId_;
+    std::int64_t timestamp_;
+    WeatherSeverity severity_;
+};
