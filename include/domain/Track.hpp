18e2df8b1f27945571a56fa2f86ca9345165a5d9
ingvildandas <ingvild.andas@outlook.com>
2026-03-05 08:27:45 +0100

    UPDATE: Track class

--- include/domain/Track.hpp
+++ include/domain/Track.hpp
@@ -1,6 +1,26 @@
 #pragma once 
+#include <cstdint>
+#include <string>
+#include "domain/types/Position.hpp"
 
 class Track {
-private: 
+
 public: 
+    Track(std::string icao, std::int64_t timestamp, Position position, double altitude, double velocity, double heading); 
+
+    std::string getIcao(); 
+    std::int64_t getTimestamp();
+    Position getPosition();
+    double getAltitude();
+    double getVelocity();
+    double getHeading();
+    
+private: 
+    std::string icao_; 
+    std::int64_t timestamp_;
+    Position position_; 
+    double altitude_; 
+    double velocity_; 
+    double heading_; 
+
 }; 
--- src/domain/Track.cpp
+++ src/domain/Track.cpp
@@ -1,1 +1,29 @@
 #include "domain/Track.hpp"
+
+Track::Track(std::string icao, std::int64_t timestamp, Position position, double altitude, double velocity, double heading) 
+    : icao_(std::move(icao)), timestamp_(timestamp), position_(std::move(position)), altitude_(altitude), velocity_(velocity), heading_(heading) {}
+
+std::string Track::getIcao() {
+    return icao_;
+}
+
+std::int64_t Track::getTimestamp() {
+    return timestamp_;
+}
+
+Position Track::getPosition() {
+    return position_;
+}
+
+double Track::getAltitude() {
+    return altitude_;
+}
+
+double Track::getVelocity() {
+    return velocity_;
+}
+
+double Track::getHeading() {
+    return heading_;
+}
+
