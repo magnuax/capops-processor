f1032f3da9f3a2324c64bb4ddb2e81c55bb6c58b
ingvildandas <ingvild.andas@outlook.com>
2026-03-16 10:11:49 +0100

    UPDATE: redis publisher

--- src/publish/RedisPublisher.cpp
+++ src/publish/RedisPublisher.cpp
@@ -1,14 +1,23 @@
 #include "publish/RedisPublisher.hpp"
 #include "publish/ProtoMapper.hpp"
 
-RedisPublisher::RedisPublisher(const Configuration &config, const Grid &grid)
-    : config_(config), grid_(grid)
+RedisPublisher::RedisPublisher(const Configuration &config) : config_(config)
 {
+    grid_ = config_.grid();
 }
 
 void RedisPublisher::publish(const ProcessingResult &result)
 {
     FlightDataProto proto = mapToProto(result, config_, grid_);
 
-    //proto.SerializeToString("serialized");
+    std::string serialized;
+    bool ok = proto.SerializeToString(&serialized);
+
+    if (!ok)
+    {
+        throw std::runtime_error("Failed to serialize FlightDataProto");
+    }
+
+    std::cout << "Serialized protobuf size: " << serialized.size() << std::endl;
+    std::cout << proto.DebugString() << std::endl;
 }
