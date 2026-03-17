57f4db7936521cf8b90af79301a83e0d0b496c41
ingvildandas <ingvild.andas@outlook.com>
2026-03-17 15:59:46 +0100

    UPDATE: Processor app

--- include/app/ProcessorApp.hpp
+++ include/app/ProcessorApp.hpp
@@ -1,9 +1,38 @@
 #pragma once
 
+#include "sources/interfaces/ITrackSource.hpp"
+#include "sources/interfaces/IWeatherSource.hpp"
+#include <memory>
+
+class RadarSimulator;
+class WeatherSimulator;
+class Configuration;
+class Grid;
+class IngestService;
+class ComputeData;
+class RedisPublisher;
+
 class ProcessorApp
 {
   public:
-    ProcessorApp() = default;
+    ProcessorApp();
+    ~ProcessorApp();
 
     void run();
+
+  private:
+    void initializeSources(const Configuration &config);
+    void tickSimulators(double timeStep);
+    void processTrackUpdates(IngestService &ingestService, ComputeData &computeData);
+    void processWeatherUpdates(const Grid &grid, IngestService &ingestService,
+                               ComputeData &computeData);
+    void publishResults(ComputeData &computeData, RedisPublisher &publisher);
+
+    std::unique_ptr<ITrackSource> trackSource_;
+    std::unique_ptr<IWeatherSource> weatherSource_;
+
+    std::unique_ptr<RadarSimulator> radarSimulator_;
+    std::unique_ptr<WeatherSimulator> weatherSimulator_;
+
+    bool simulationMode_ = false;
 };
