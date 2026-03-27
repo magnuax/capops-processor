40588fb63b43bb2d44cb1e71b412f89f6f979685
ingvildandas <ingvild.andas@outlook.com>
2026-03-27 12:10:49 +0100

    UPDATE : Remove debugging

--- src/publish/RedisPublisher.cpp
+++ src/publish/RedisPublisher.cpp
@@ -19,40 +19,5 @@ void RedisPublisher::publish(const ProcessingResult &result)
         throw std::runtime_error("Failed to serialize FlightDataProtof");
     }
 
-    // std::cout << "\n--- PUBLISH UPDATE ---\n";
-
-    // // Tracks
-    // std::cout << "\nTracks (" << result.tracks.size() << "):\n";
-    // std::cout << std::fixed << std::setprecision(5);
-
-    // for (const auto &track : result.tracks)
-    // {
-    //     const auto &pos = track.getPosition();
-
-    //     std::cout << "  " << track.getIcao() << " @ (" << pos.latDeg << ", " << pos.lonDeg << ")"
-    //               << " time=" << track.getTimestamp() << "\n";
-    // }
-
-    // // Sector summaries
-    // std::cout << "\nSectors:\n";
-
-    // for (const auto &sector : result.sectorSummaries)
-    // {
-    //     std::cout << "  Sector " << sector.getSectorId()
-    //               << " | aircraft=" << sector.getLocalAircraftCount()
-    //               << " | weather=" << weatherSeverityToString(sector.getWeatherSeverity())
-    //               << " | state=" << sectorStateToString(sector.getState()) << "\n";
-    // }
-
-    // // Risk events
-    // std::cout << "\nRiskEvents (" << result.riskEvents.size() << "):\n";
-
-    // for (const auto &event : result.riskEvents)
-    // {
-    //     std::cout << "  " << event.getMessage() << "\n";
-    // }
-
-    // std::cout << "----------------------\n";
-
     redis_.publish(channel_, serialized);
 }
